e7f3663a32b09a1203c90a74533fde32f01754ba
carterx64 <carterrahn96@gmail.com>
2024-08-27 18:28:52 -0400

    CS 201 Folder Contents

---
+++ practice.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    ifstream file("C:/Users/carahn/Desktop/CS 201/test");
+    if (!file.is_open()) {
+        cout << "Failed to open the file." << endl;
+        return 1;
+    }
+
+    int lineCount = 0;
+    char character;
+
+    while (file.get(character)) {
+        if (character == '\n') {
+            ++lineCount;
+        }
+    }
+
+    cout << lineCount << endl;
+    return 0;
+}
